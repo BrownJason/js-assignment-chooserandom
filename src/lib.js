a0c5f5f10616888acdb02291c360c389d1159502
BrownJason <jbjbro6@gmail.com>
2018-03-27 23:24:35 -0500

    kinda working..

--- src/lib.js
+++ src/lib.js
@@ -7,23 +7,22 @@ export const chooseRandom = (array = [], numItems) => {
         numItems = Math.floor(Math.random() * array.length)
       }
       const randomIndices = Array(numItems).fill().reduce((res, next) => {
-        for (let i = 0; i < Array.length; i++) {
-          next = array[Math.floor(Math.random() * array.length)]
-          if (res.includes(next)) {
-            i--
-          } else {
-            res.push(next)
-          }
+        next = array[Math.floor(Math.random() * array.length)]
+        if (!res.includes(next)) {
+          res.push(next)
         }
         return res
       }, [])
 
       const checkArray = array.filter(function (index) {
         if (array.includes(index) === randomIndices.includes(index)) {
-          return true
+          return randomIndices.includes(index)
         }
       })
+      console.log(checkArray)
       return checkArray
+    } else {
+      return array
     }
   } else {
     return array

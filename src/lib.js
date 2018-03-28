119569ba44c183593b4efb40f6f7ec2ef5eb758f
BrownJason <jbjbro6@gmail.com>
2018-03-28 08:53:56 -0500

    Not purely random - but returns correct amount

--- src/lib.js
+++ src/lib.js
@@ -8,19 +8,15 @@ export const chooseRandom = (array = [], numItems) => {
       }
       const randomIndices = Array(numItems).fill().reduce((res, next) => {
         next = array[Math.floor(Math.random() * array.length)]
-        if (!res.includes(next)) {
-          res.push(next)
-        }
+        res.push(next)
         return res
       }, [])
 
-      const checkArray = array.filter(function (index) {
+      return randomIndices.filter(function (index) {
         if (array.includes(index) === randomIndices.includes(index)) {
           return randomIndices.includes(index)
         }
       })
-      console.log(checkArray)
-      return checkArray
     } else {
       return array
     }
